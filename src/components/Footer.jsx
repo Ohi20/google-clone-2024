5c1ad2be5b2025ccb309816846a4cf6f8e1ee529
AshrafOhi <ashrafohi20@gmail.com>
2024-03-16 15:47:25 +0600

    links-added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import CountryLookup from "./CountryLookup";
 
 export default function Footer() {
@@ -8,15 +9,57 @@ export default function Footer() {
       </div>
       <div className="flex flex-col sm:flex-row justify-between items-center px-8 py-3 space-y-7 sm:space-y-0">
         <ul className="flex items-center space-x-6">
-          <li className="hover:underline cursor-pointer">About</li>
-          <li className="hover:underline cursor-pointer">Advertising</li>
-          <li className="hover:underline cursor-pointer">Business</li>
-          <li className="hover:underline cursor-pointer">How Search works</li>
+          <Link
+            href="https://about.google/"
+            className="hover:underline cursor-pointer"
+            target="_blank"
+          >
+            About
+          </Link>
+          <Link
+            href="https://ads.google.com/intl/en_all/start/overview/?subid=bd-en-ha-awa-bk-a-b00!o3~Cj0KCQjwhtWvBhD9ARIsAOP0GojtubK1t1G8lFjGrCMtEOhoHaVzjPPlzylUwdiLZm5V5CZGgEAV8okaAmehEALw_wcB~139027759156~kwd-21297553~17792962241~611031582696&gad_source=1&gclid=Cj0KCQjwhtWvBhD9ARIsAOP0GojtubK1t1G8lFjGrCMtEOhoHaVzjPPlzylUwdiLZm5V5CZGgEAV8okaAmehEALw_wcB&gclsrc=aw.ds#"
+            className="hover:underline cursor-pointer"
+            target="_blank"
+          >
+            Advertising
+          </Link>
+          <Link
+            href="https://ads.google.com/intl/en_all/start/overview/?subid=bd-en-ha-awa-bk-a-b00!o3~Cj0KCQjwhtWvBhD9ARIsAOP0GojtubK1t1G8lFjGrCMtEOhoHaVzjPPlzylUwdiLZm5V5CZGgEAV8okaAmehEALw_wcB~139027759156~kwd-21297553~17792962241~611031582696&gad_source=1&gclid=Cj0KCQjwhtWvBhD9ARIsAOP0GojtubK1t1G8lFjGrCMtEOhoHaVzjPPlzylUwdiLZm5V5CZGgEAV8okaAmehEALw_wcB&gclsrc=aw.ds#"
+            className="hover:underline cursor-pointer"
+            target="_blank"
+          >
+            Business
+          </Link>
+          <Link
+            href="https://www.google.com/search/howsearchworks/how-search-works/"
+            className="hover:underline cursor-pointer"
+            target="_blank"
+          >
+            How Search works
+          </Link>
         </ul>
         <ul className="flex items-center space-x-6">
-          <li className="hover:underline cursor-pointer">Privacy</li>
-          <li className="hover:underline cursor-pointer">Terms</li>
-          <li className="hover:underline cursor-pointer">Settings</li>
+          <Link
+            href="https://policies.google.com/privacy?hl=en-US"
+            className="hover:underline cursor-pointer"
+            target="_blank"
+          >
+            Privacy
+          </Link>
+          <Link
+            href="https://policies.google.com/terms?hl=en-US"
+            className="hover:underline cursor-pointer"
+            target="_blank"
+          >
+            Terms
+          </Link>
+          <Link
+            href="https://support.google.com/accounts/answer/3118621?hl=en"
+            className="hover:underline cursor-pointer"
+            target="_blank"
+          >
+            Settings
+          </Link>
         </ul>
       </div>
     </footer>
